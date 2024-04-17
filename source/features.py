393983e6038136e5b1d28755053ed503c78b6b3e
Albert Saiapin <analystalb@gmail.com>
2024-04-17 16:38:13 +0200

    added features for quantized method

--- source/features.py
+++ source/features.py
@@ -1,8 +1,11 @@
 from typing import Callable
+from collections import namedtuple
 
 import numpy as np
 
-FeatureMap = Callable[[np.array], np.array]
+FeatureMap = Callable[..., np.array]
+PPQ2Feature = namedtuple('PPQ2Feature', 'name', defaults=['pure_poly'])
+RFQ2Feature = namedtuple('RFQ2Feature', 'lscale, name', defaults=[1, 'rbf_fourier'])
 
 def pure_poli_features(x: np.array, order: int) -> np.array:
     """ Calculate pure polinomial features matrix for x. """
@@ -24,3 +27,22 @@ def gaussian_kernel_features(
     w_scaled = np.pi * np.arange(1, order + 1) / (2 * domain_bound)
     sd = np.sqrt(2 * np.pi) * lscale * np.exp(-np.power(lscale * w_scaled, 2) / 2)
     return np.sqrt(sd / domain_bound) * np.sin(np.outer(x, w_scaled)) 
+
+def q2_poli_features(x: np.array, q: int) -> np.array:
+    """ Calculate special quantized pure polinomial features matrix for x. """
+    return np.power(x[:, None], [0, 2**q])
+
+def q2_fourier_features(
+    x: np.array, 
+    q: int, 
+    m_order: int, 
+    k_d: int, 
+    lscale: float = 1
+) -> np.array:
+    """ Calculate special quantized Gaussian (squared exp.) kernel features matrix for x. """
+    return np.hstack(
+        (
+            np.exp(-1j * np.pi * x * m_order / (k_d * lscale))[:, None], 
+            np.exp(1j * np.pi * (-x * m_order / k_d + x*(2**q)) / lscale)[:, None]
+        ),
+    )
