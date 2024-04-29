a1852cb90f9a03cc7b35e44388ad5e8000bebe1d
Albert Saiapin <analystalb@gmail.com>
2024-04-29 15:40:26 +0200

    fixed types; added dummy variable

--- source/features.py
+++ source/features.py
@@ -3,20 +3,25 @@ from collections import namedtuple
 
 import numpy as np
 
-FeatureMap = Callable[..., np.array]
+FeatureMap = Callable[..., np.ndarray]
 PPQ2Feature = namedtuple('PPQ2Feature', 'name', defaults=['pure_poly'])
 RFQ2Feature = namedtuple('RFQ2Feature', 'lscale, name', defaults=[1, 'rbf_fourier'])
 
-def pure_poli_features(x: np.array, order: int) -> np.array:
+def pure_poli_features(
+    x: np.ndarray, 
+    q: int, # Dummy
+    order: int
+) -> np.ndarray:
     """ Calculate pure polinomial features matrix for x. """
     return np.power(x[:, None], np.arange(order))
 
 def gaussian_kernel_features(
-        x: np.array, 
-        order: int, 
-        lscale: float = 1, 
-        domain_bound: float = 1,
-    ) -> np.array:
+    x: np.ndarray,
+    q: int,  # Dummy
+    order: int, 
+    lscale: float = 1, 
+    domain_bound: float = 1,
+) -> np.ndarray:
     """ 
     Calculate Gaussian (squared exp.) kernel features matrix for x. 
 
@@ -28,17 +33,17 @@ def gaussian_kernel_features(
     sd = np.sqrt(2 * np.pi) * lscale * np.exp(-np.power(lscale * w_scaled, 2) / 2)
     return np.sqrt(sd / domain_bound) * np.sin(np.outer(x, w_scaled)) 
 
-def q2_poli_features(x: np.array, q: int) -> np.array:
+def q2_poli_features(x: np.ndarray, q: int) -> np.ndarray:
     """ Calculate special quantized pure polinomial features matrix for x. """
     return np.power(x[:, None], [0, 2**q])
 
 def q2_fourier_features(
-    x: np.array, 
+    x: np.ndarray, 
     q: int, 
     m_order: int, 
     k_d: int, 
     lscale: float = 1
-) -> np.array:
+) -> np.ndarray:
     """ Calculate special quantized Gaussian (squared exp.) kernel features matrix for x. """
     return np.hstack(
         (
