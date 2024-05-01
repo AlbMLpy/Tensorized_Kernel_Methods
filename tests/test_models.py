fa2e7dfed3409761e84960ca90405b48ffcdea40
Albert Saiapin <analystalb@gmail.com>
2024-05-01 10:44:36 +0200

    added test for FL model

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,7 +9,10 @@ sys.path.append('./')
 
 from source.models.CPR import CPR
 from source.models.QCPR import QCPR
+from source.models.QCPRf import QCPRf
+from source.features import PPQ2Feature, RFQ2Feature
 from source.general_functions import prepare_callback
+
 
 class TestModels(unittest.TestCase):
     def setUp(self) -> None:
@@ -23,7 +26,7 @@ class TestModels(unittest.TestCase):
         model_params = dict(
             rank=8,
             m_order=4,
-            n_epoch=32,
+            n_epoch=20,
             alpha=0.001,
             random_state=0,
             callback=callback_function,
@@ -40,12 +43,35 @@ class TestModels(unittest.TestCase):
         model_params = dict(
             rank=8,
             m_order=4,
-            n_epoch=32,
+            n_epoch=20,
             alpha=0.001,
             random_state=0,
             callback=callback_function,
         )
         model = QCPR(**model_params)
+        model.fit(self.x, self.y) 
+
+        expected = sorted(callback_function.data, reverse=True)
+        actual = callback_function.data
+        self.assertTrue(np.allclose(actual, expected))
+
+    
+    def test_qcprf_loss(self):
+        callback_function = prepare_callback()
+        model_params = dict(
+            rank=8,
+            m_order=4,
+            fmaps_list=[
+                PPQ2Feature(), 
+                RFQ2Feature(lscale=1), 
+            ],
+            n_epoch=20,
+            alpha=0.001,
+            beta=0.001,
+            random_state=0,
+            callback=callback_function,
+        )
+        model = QCPRf(**model_params)
         model.fit(self.x, self.y) 
 
         expected = sorted(callback_function.data, reverse=True)
