9e5be34c0fad0aec5696811ee1441d6e9a768809
Albert Saiapin <analystalb@gmail.com>
2024-05-24 17:03:14 +0200

    Q non-Q correspondence

--- tests/test_features.py
+++ tests/test_features.py
@@ -6,11 +6,13 @@ import numpy as np
 sys.path.append('./')
 
 from source.features import (
+    ff_q2,
+    ppf_q2,
+    fourier_features,
     pure_poli_features, 
     gaussian_kernel_features,
-    ppf_q2,
-    ff_q2,
 )
+from source.matrix_operations import khatri_rao_row
 
 class TestFeatures(unittest.TestCase):
     def test_pure_poli_features(self):
@@ -72,4 +74,28 @@ class TestFeatures(unittest.TestCase):
             k_d=3,
             p_scale=1
         )
+        self.assertTrue(np.allclose(actual, expected))
+
+    def test_q_nq_poli_features(self):
+        x = np.array([1, 2, 3])
+        m_order = 256
+        k_d = int(np.log2(m_order))
+
+        expected = pure_poli_features(x, None, m_order)
+        actual = ppf_q2(x, 0)
+        for q in range(1, k_d):
+            actual = khatri_rao_row(ppf_q2(x, q), actual)
+        self.assertTrue(np.allclose(actual, expected))
+
+    def test_q_nq_fourier_features(self):
+        x = np.array([0.1, 0.2, 1, 20])
+        m_order = 2**8
+        p_scale = 20
+        k_d = int(np.log2(m_order))
+
+        actual = ff_q2(x, 0, m_order, k_d, p_scale)
+        for q in range(1, k_d):
+            actual = khatri_rao_row(ff_q2(x, q, m_order, k_d, p_scale), actual)
+
+        expected = fourier_features(x, None, m_order, p_scale)
         self.assertTrue(np.allclose(actual, expected))
