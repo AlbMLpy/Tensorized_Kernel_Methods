5d10567f6633ee1dd4ad7a929640a5658af15db8
Albert Saiapin <analystalb@gmail.com>
2024-05-24 09:53:39 +0200

    helping functions for data processing

--- source/data_functions.py
+++ source/data_functions.py
@@ -11,9 +11,10 @@ def print_df_stats(df: pd.DataFrame, save_path=None) -> None:
     task = 'Classification' if 'class' in df.columns else 'Regression'
     print(
         f'# Samples = {n_samples}\n'
-        f'# Features = {n_features}\n'
+        f'# Features = {n_features - 1}\n'
         f'Task: {task}\n'
     )
+    print(df.head(3))
     if save_path is not None:
         pd.Series(
             {
