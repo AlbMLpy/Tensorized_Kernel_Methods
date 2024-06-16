eb20c8013408fdf659f7d62094b8bdd67a53c9a3
Albert Saiapin <analystalb@gmail.com>
2024-06-16 22:05:38 +0200

    added new parameters

--- source/models/QCPRf.py
+++ source/models/QCPRf.py
@@ -6,21 +6,25 @@ from sklearn.metrics import r2_score
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
-from ..features import PPQ2Feature, q2_poli_features, q2_fourier_features
+from ..features import Feature, PPFeature, ppf_q2, ff_q2
 from ..q_cpr_f import q_cpr_f, predict_score
 
-class QCPRf(BaseEstimator, RegressorMixin):
+class QCPRf(RegressorMixin, BaseEstimator):
     """ Quantized CP Regression Model with feature relevance learning (QCPRf). """
     def __init__(
         self, 
         rank: int = 1, 
-        fmaps_list: list[tuple] = [PPQ2Feature(),],
+        fmaps_list: list[Feature] = [PPFeature(),],
         m_order: int = 2,
         init_type: str = 'kj_vec',
         n_epoch: int = 1, 
-        alpha: int = 1, 
-        beta: int = 1,
+        alpha: float = 1.0, 
+        beta: float = 1.0,
+        lambda_reg_type: str = 'l2',
+        n_steps_l1: int = 100,
         random_state: Optional[int] = None,
+        init_equal_lambda: bool = False,
+        positive_lambda: bool = False,
         callback: Optional[Callable] = None,
     ):
         _check_input_params(m_order)  
@@ -31,22 +35,26 @@ class QCPRf(BaseEstimator, RegressorMixin):
         self.n_epoch = n_epoch
         self.alpha = alpha
         self.beta = beta
+        self.lambda_reg_type = lambda_reg_type
+        self.n_steps_l1 = n_steps_l1
         self.random_state = random_state
+        self.init_equal_lambda = init_equal_lambda
+        self.positive_lambda = positive_lambda
         self.callback = callback
         self._dtype = None
     
     def _prepare_feature_mappings(self):
         mappings = []
         for feature in self.fmaps_list:
-            if feature.name == 'pure_poly':
-                mappings.append(q2_poli_features)
-            elif feature.name == 'rbf_fourier':
+            if feature.name == 'ppf':
+                mappings.append(ppf_q2)
+            elif feature.name == 'ff':
                 mappings.append(
                     partial(
-                        q2_fourier_features, 
+                        ff_q2, 
                         m_order=self.m_order, 
                         k_d=int(np.log2(self.m_order)), 
-                        lscale=feature.lscale,
+                        p_scale=feature.p_scale,
                     )
                 )
                 self._dtype = np.complex128
@@ -62,7 +70,8 @@ class QCPRf(BaseEstimator, RegressorMixin):
         self.weights_, self.lambdas_, self.kd_ = q_cpr_f(
             X, y, self.m_order, self._feature_maps_list, 
             self.rank, self.init_type, self.n_epoch, self.alpha, 
-            self.beta, self.random_state, self._dtype, self.callback
+            self.beta, self.lambda_reg_type, self.n_steps_l1, self.random_state, 
+            self.init_equal_lambda, self.positive_lambda, self._dtype, self.callback,
         )
         self.is_fitted_ = True
         return self
@@ -81,3 +90,4 @@ class QCPRf(BaseEstimator, RegressorMixin):
 def _check_input_params(m_order: int) -> None:
     if m_order & (m_order - 1):
         raise ValueError(f"m_order should be a power of 2, but it is {m_order}.")
+    
