9ddbf01e9ecd3a47e9ed5bd13c4e232f8c991e6a
Albert Saiapin <analystalb@gmail.com>
2024-07-24 12:02:57 +0200

    added more functionality: update order; fixed norm opt.

--- source/q_cpr_f.py
+++ source/q_cpr_f.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Optional, Callable
 
 import numpy as np
@@ -12,7 +13,7 @@ from .model_functionality import (
 )
 from .cpr import Q_BASE
 from .general_functions import performance_decorator
-from .optimization import fista, ls_solution
+from .optimization import fista, ls_solution, lsc_solution
 
 PERF_DECORATOR_ENABLED = False
 _performance_buffer = {}
@@ -114,9 +115,12 @@ def update_feature_weights(
         lambdas = ls_solution(f_mtx, y, beta)
     elif reg_type == 'l1':
         lambdas = fista(f_mtx, y, lambdas, beta, n_steps=n_steps_l1)
+    elif reg_type == 'fixed_norm':
+        lambdas = lsc_solution(f_mtx, y, alp=1) # fixed l2 norm = 1
     else:
-        raise NotImplementedError("Choose 'l1' or 'l2'")
-    if positive:
+        raise NotImplementedError("Choose 'l1' or 'l2' or 'fixed_norm'")
+    if positive: # THIS IS PROBABLY NOT RIGHT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        raise NotImplementedError('Positive lambdas are not implemented yet')
         lambdas = np.maximum(lambdas, 0.0)
     return lambdas
 
@@ -130,28 +134,37 @@ def q_cpr_f(
     n_epoch: int,
     alpha: float,
     beta: float,
-    lambda_reg_type: str = 'l2',
-    n_steps_l1: int = 100,
+    l_reg: str = 'l2',
+    ns_l1: int = 100,
     seed: Optional[int] = None,
-    init_equal_lambda: bool = False,
-    positive_lambda: bool = False,
+    l_init_eq: bool = False,
+    l_pos: bool = False,
     dtype: np.dtype = np.float64,
+    xy_test: Optional[tuple] = None,
     callback: Optional[Callable] = None,
+    upd_t: str = 'wl',
 ) -> tuple[np.ndarray, np.ndarray, int]:
-    """ TODO """
     weights, lambdas, k_d, fwh_matrices, ww_hadamard = prepare_modeling(
-        x, m_order, fmaps_list, rank, init_type, seed, init_equal_lambda, dtype,
+        x, m_order, fmaps_list, rank, init_type, seed, l_init_eq, dtype,
     )
-    run_callback(x, y, k_d, weights, lambdas, fmaps_list, alpha, beta, callback)
+    rc = partial(run_callback, x=x, y=y, k_d=k_d, feature_maps_list=fmaps_list, 
+        alpha=alpha, beta=beta, xy_test=xy_test, callback=callback)
+    rc(weights=weights, lambdas=lambdas)
     for _ in range(n_epoch):
-        ### TRAIN W ###
-        weights = update_quantized_weights(
-            x, y, alpha, k_d, weights, lambdas, fmaps_list, fwh_matrices, ww_hadamard)
-        run_callback(x, y, k_d, weights, lambdas, fmaps_list, alpha, beta, callback)
-        ### TRAIN L ###
-        lambdas = update_feature_weights(x, y, beta, k_d, weights, lambdas, 
-            fmaps_list, lambda_reg_type, n_steps_l1, positive_lambda)
-        run_callback(x, y, k_d, weights, lambdas, fmaps_list, alpha, beta, callback)
+        if upd_t == 'wl':
+            weights = update_quantized_weights(
+                x, y, alpha, k_d, weights, lambdas, fmaps_list, fwh_matrices, ww_hadamard)
+            rc(weights=weights, lambdas=lambdas)
+            lambdas = update_feature_weights(x, y, beta, k_d, weights, lambdas, 
+                fmaps_list, l_reg, ns_l1, l_pos)
+            rc(weights=weights, lambdas=lambdas)
+        else:
+            lambdas = update_feature_weights(x, y, beta, k_d, weights, lambdas, 
+                fmaps_list, l_reg, ns_l1, l_pos)
+            rc(weights=weights, lambdas=lambdas)
+            weights = update_quantized_weights(
+                x, y, alpha, k_d, weights, lambdas, fmaps_list, fwh_matrices, ww_hadamard)
+            rc(weights=weights, lambdas=lambdas)
         if not lambdas.any(): # if all lambda values are zeros - stop optimization
             break
     return weights, lambdas, k_d
@@ -178,8 +191,14 @@ def run_callback(
         feature_maps_list, 
         alpha,
         beta, 
+        xy_test: Optional[tuple] = None,
         callback: Optional[Callable] = None,
 ):
     if callback:
+        y_yp = None
+        if xy_test:
+            x_test, y_test = xy_test
+            y_pred_test = predict_score(x_test, k_d, weights, lambdas, feature_maps_list)
+            y_yp = y_test, y_pred_test
         y_pred = predict_score(x, k_d, weights, lambdas, feature_maps_list)
-        callback(y, y_pred, k_d, weights, lambdas, feature_maps_list, alpha, beta)
+        callback(y, y_pred, k_d, weights, lambdas, feature_maps_list, alpha, beta, y_yp)
