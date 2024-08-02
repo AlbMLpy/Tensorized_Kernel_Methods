06ccb39517b7e29a8ed125cf6eb6eeab421655f9
Albert Saiapin <analystalb@gmail.com>
2024-08-02 10:33:41 +0200

    jump out of the loop by norm; positive sol;

--- source/optimization.py
+++ source/optimization.py
@@ -6,21 +6,33 @@ from scipy.optimize import newton
 def shrinkage_operator(x: np.ndarray, alpha: float) -> np.ndarray:
     return np.sign(x) * np.maximum((np.abs(x) - alpha), 0)
 
+def shrinkage_operator_p(x: np.ndarray, alpha: float) -> np.ndarray:
+    return np.maximum((np.abs(x) - alpha), 0)
+
 def fista( 
     x: np.ndarray, 
     y: np.ndarray,
     w: np.ndarray,
     beta: float,
     n_steps: int,
+    atol: float = 1e-4,
+    pos: bool = False,
 ) -> np.ndarray:
     a_mtx, b = x.T.conj().dot(x), x.T.conj().dot(y)
     yk, tk_prev = 1*w, 1
     ss = 1 / np.linalg.norm(a_mtx, ord=2)
+    n1 = np.linalg.norm(w, ord=2)
+    sh = shrinkage_operator_p if pos else shrinkage_operator
     for _ in range(n_steps):
-        w = shrinkage_operator(yk - ss*(a_mtx.dot(yk) - b), beta * ss)
+        w = sh(yk - ss*(a_mtx.dot(yk) - b), beta * ss)
         tk = 0.5 * (1 + np.sqrt(1 + 4*tk_prev**2))
         yk = w + (tk_prev - 1) / tk * (w - yk)
         tk_prev = tk
+        n2 = np.linalg.norm(w, ord=2)
+        if np.isclose(n1, n2, rtol=0, atol=atol):
+            break
+        else:
+            n1 = n2
     return w
 
 ### L2 Regularization optimization ###
@@ -58,5 +70,5 @@ def lsc_solution(
     u, s, vt = svd(x, full_matrices=False)
     c = u.T.dot(y)
     ss, sc = s**2, s*c
-    mu = newton(nl_mu, mu0, args=(ss, sc, alp))
+    mu = newton(nl_mu, mu0, args=(ss, sc, alp), disp=False) # THINK ABOUT THIS ONE 
     return vt.T.dot(sc / (ss + 2*mu))
