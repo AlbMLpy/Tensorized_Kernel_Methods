321d3b10ab7b63576be38a3e35eb42f2d8f24621
Albert Saiapin <analystalb@gmail.com>
2024-08-02 14:07:53 +0200

    refactored; L uniform or ones init;

--- source/q_cpr_f.py
+++ source/q_cpr_f.py
@@ -15,6 +15,7 @@ from .cpr import Q_BASE
 from .general_functions import performance_decorator
 from .optimization import fista, ls_solution, lsc_solution
 
+N_INTERNAL_STEPS = 3
 PERF_DECORATOR_ENABLED = False
 _performance_buffer = {}
 
@@ -25,22 +26,21 @@ def init_feature_weights(
     dtype: np.dtype = np.float64,
 ) -> np.ndarray:
     random_state = np.random if seed is None else np.random.RandomState(seed) 
-    weights = np.full((n_values, 1), random_state.randn(1))[:, 0] if init_equal else random_state.randn(n_values) 
+    weights = np.ones(n_values) if init_equal else random_state.rand(n_values) 
     return weights.astype(dtype)
 
 def precalculate_step(
     x: np.ndarray,
     k_d: int,
     weights: np.ndarray,
-    feature_maps_list: list[FeatureMap],
+    fmaps_list: list[FeatureMap],
     dtype: np.dtype = np.float64
 ) -> tuple[np.ndarray, np.ndarray]:
-    shape = (len(feature_maps_list), x.shape[0], weights.shape[-1])
+    shape = (len(fmaps_list), x.shape[0], weights.shape[-1])
     fwh_matrices = np.empty(shape, dtype=weights.dtype)
-    for p, feature_map in enumerate(feature_maps_list):
+    for p, feature_map in enumerate(fmaps_list):
         fwh_matrices[p] = get_fw_hadamard_mtx(x, k_d, weights, feature_map, dtype)
-    ww_hadamard = get_ww_hadamard_mtx(weights, dtype)
-    return fwh_matrices, ww_hadamard
+    return fwh_matrices, get_ww_hadamard_mtx(weights, dtype)
 
 @performance_decorator(enabled=PERF_DECORATOR_ENABLED, buffer=_performance_buffer)
 def prepare_modeling(
@@ -67,17 +67,17 @@ def update_quantized_weights(
     k_d: int,
     weights: np.ndarray,
     lambdas: np.ndarray,
-    feature_maps_list: list[FeatureMap],
+    fmaps_list: list[FeatureMap],
     fwh_matrices: np.ndarray,
     ww_hadamard: np.ndarray,
 ) -> np.ndarray:
     (n, _), (q_base, rank) = x.shape, weights.shape[-2:]
-    for ind in range(weights.shape[0]):
+    for ind in range(weights.shape[0]): 
         k, q = divmod(ind, k_d) # q starts from zero -> for feature_map
         wk, Fk = weights[ind], np.zeros((n, q_base*rank), dtype=weights.dtype)
         # Preprocess:
         ww_hadamard /= wk.T.conj().dot(wk)
-        for p, feature_map in enumerate(feature_maps_list):
+        for p, feature_map in enumerate(fmaps_list):
             if lambdas[p]: # can be zero
                 fk_mtx = feature_map(x[:, k], q)
                 fwh_matrices[p] /= fk_mtx.dot(wk) # remove k-th factor
@@ -89,11 +89,11 @@ def update_quantized_weights(
         wk = np.linalg.solve(A, b).reshape(q_base, rank, order='F') # Fortran Ordering
         # Postprocess:
         ww_hadamard *= wk.T.conj().dot(wk)
-        for p, feature_map in enumerate(feature_maps_list):
+        for p, feature_map in enumerate(fmaps_list):
             fk_mtx = feature_map(x[:, k], q)
             fwh_matrices[p] *= fk_mtx.dot(wk)
         weights[ind] = wk
-    return weights
+    return weights, lambdas
 
 @performance_decorator(enabled=PERF_DECORATOR_ENABLED, buffer=_performance_buffer)
 def update_feature_weights( 
@@ -103,26 +103,24 @@ def update_feature_weights(
     k_d: int,
     weights: np.ndarray, 
     lambdas: np.ndarray,
-    feature_maps_list: list[FeatureMap],
-    reg_type: str = 'l2',
-    n_steps_l1: int = 1000,
-    positive: bool = False,
+    fmaps_list: list[FeatureMap],
+    l_reg: str = 'l2',
+    ns_l1: int = 1000,
+    l_pos: bool = False,
 ) -> np.ndarray:
     f_mtx = np.empty((x.shape[0], len(lambdas)))
-    for p, feature_map in enumerate(feature_maps_list):
+    for p, feature_map in enumerate(fmaps_list):
         f_mtx[:, p] = predict_score_fm(x, k_d, weights, feature_map)
-    if reg_type == 'l2':
+    if l_reg == 'l2':
         lambdas = ls_solution(f_mtx, y, beta)
-    elif reg_type == 'l1':
-        lambdas = fista(f_mtx, y, lambdas, beta, n_steps=n_steps_l1)
-    elif reg_type == 'fixed_norm':
+    elif l_reg == 'l1':
+        if len(np.nonzero(lambdas)[0]) > 1: # do not update if only 1 left
+            lambdas = fista(f_mtx, y, lambdas, beta, n_steps=ns_l1, pos=l_pos)
+    elif l_reg == 'fixed_norm':
         lambdas = lsc_solution(f_mtx, y, alp=1) # fixed l2 norm = 1
     else:
         raise NotImplementedError("Choose 'l1' or 'l2' or 'fixed_norm'")
-    if positive: # THIS IS PROBABLY NOT RIGHT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        raise NotImplementedError('Positive lambdas are not implemented yet')
-        lambdas = np.maximum(lambdas, 0.0)
-    return lambdas
+    return weights, lambdas
 
 def q_cpr_f(
     x: np.ndarray, 
@@ -145,28 +143,26 @@ def q_cpr_f(
     upd_t: str = 'wl',
 ) -> tuple[np.ndarray, np.ndarray, int]:
     weights, lambdas, k_d, fwh_matrices, ww_hadamard = prepare_modeling(
-        x, m_order, fmaps_list, rank, init_type, seed, l_init_eq, dtype,
+        x, m_order, fmaps_list, rank, init_type, seed, l_init_eq, dtype
     )
-    rc = partial(run_callback, x=x, y=y, k_d=k_d, feature_maps_list=fmaps_list, 
-        alpha=alpha, beta=beta, xy_test=xy_test, callback=callback)
+    _shared = dict(x=x, y=y, k_d=k_d, fmaps_list=fmaps_list)
+    rc = partial(run_callback, alpha=alpha, beta=beta, 
+        xy_test=xy_test, callback=callback, **_shared
+    )
+    mode2f = {
+        0: partial(update_quantized_weights, alpha=alpha, fwh_matrices=fwh_matrices, 
+            ww_hadamard=ww_hadamard, **_shared),
+        1: partial(update_feature_weights, beta=beta, l_reg=l_reg, ns_l1=ns_l1, 
+            l_pos=l_pos, **_shared),
+    }
+    n_iter = N_INTERNAL_STEPS if upd_t in ['swsl', 'slsw'] else 1
+    modes = [0, 1] if upd_t in ['wl', 'swsl'] else [1, 0]
     rc(weights=weights, lambdas=lambdas)
-    for _ in range(n_epoch):
-        if upd_t == 'wl':
-            weights = update_quantized_weights(
-                x, y, alpha, k_d, weights, lambdas, fmaps_list, fwh_matrices, ww_hadamard)
-            rc(weights=weights, lambdas=lambdas)
-            lambdas = update_feature_weights(x, y, beta, k_d, weights, lambdas, 
-                fmaps_list, l_reg, ns_l1, l_pos)
-            rc(weights=weights, lambdas=lambdas)
-        else:
-            lambdas = update_feature_weights(x, y, beta, k_d, weights, lambdas, 
-                fmaps_list, l_reg, ns_l1, l_pos)
-            rc(weights=weights, lambdas=lambdas)
-            weights = update_quantized_weights(
-                x, y, alpha, k_d, weights, lambdas, fmaps_list, fwh_matrices, ww_hadamard)
-            rc(weights=weights, lambdas=lambdas)
-        if not lambdas.any(): # if all lambda values are zeros - stop optimization
-            break
+    for _ in range(n_epoch//n_iter):
+        for i in modes:
+            for _ in range(n_iter):
+                weights, lambdas = mode2f[i](weights=weights, lambdas=lambdas)
+                rc(weights=weights, lambdas=lambdas)
     return weights, lambdas, k_d
 
 def predict_score(
@@ -174,31 +170,21 @@ def predict_score(
     k_d: int,
     weights: np.ndarray, 
     lambdas: np.ndarray,
-    feature_maps_list: list[FeatureMap],
+    fmaps_list: list[FeatureMap],
 ) -> np.ndarray:
     score = np.zeros(x.shape[0])
-    for p, feature_map in enumerate(feature_maps_list):
+    for p, feature_map in enumerate(fmaps_list):
         if lambdas[p]:
             score += lambdas[p] * predict_score_fm(x, k_d, weights, feature_map)
     return score
 
 def run_callback(
-        x, 
-        y, 
-        k_d: int,
-        weights, 
-        lambdas, 
-        feature_maps_list, 
-        alpha,
-        beta, 
-        xy_test: Optional[tuple] = None,
-        callback: Optional[Callable] = None,
+    x, y, k_d: int, weights, lambdas, fmaps_list, alpha, beta, 
+    xy_test: Optional[tuple] = None, callback: Optional[Callable] = None
 ):
     if callback:
         y_yp = None
         if xy_test:
-            x_test, y_test = xy_test
-            y_pred_test = predict_score(x_test, k_d, weights, lambdas, feature_maps_list)
-            y_yp = y_test, y_pred_test
-        y_pred = predict_score(x, k_d, weights, lambdas, feature_maps_list)
-        callback(y, y_pred, k_d, weights, lambdas, feature_maps_list, alpha, beta, y_yp)
+            y_yp = xy_test[1], predict_score(xy_test[0], k_d, weights, lambdas, fmaps_list)
+        y_pred = predict_score(x, k_d, weights, lambdas, fmaps_list)
+        callback(y, y_pred, k_d, weights, lambdas, fmaps_list, alpha, beta, y_yp)
